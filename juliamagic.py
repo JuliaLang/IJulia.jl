876a7f5c5893fc277aeb6db11eb6aa9836756cac
Fernando Perez <Fernando.Perez@berkeley.edu>
2013-03-01 14:49:08 -0500

    Add new api version

--- juliamagic.py
+++ juliamagic.py
@@ -23,6 +23,70 @@ class JuliaMagicError(Exception):
 
 
 @magics_class
+class JuliaMagics0(Magics):
+    """A set of magics useful for interactive work with Julia.
+    """
+    def __init__(self, shell):
+        """
+        Parameters
+        ----------
+        shell : IPython shell
+
+        """
+
+        global _julia_initialized
+        
+        super(JuliaMagics0, self).__init__(shell)
+
+        # Ugly hack to register the julia interpreter globally so we can reload
+        # this extension without trying to re-open the shared lib, which kills
+        # the python interpreter.  Nasty but useful while debugging
+        if hasattr(sys, '_julia_initialized'):
+            j = sys._julia_initialized
+        else: 
+            j = ctypes.CDLL('libjulia-release.so', ctypes.RTLD_GLOBAL)
+            j.jl_init("/home/fperez/tmp/src/julia/usr/lib")
+            sys._julia_initialized = j
+       
+        j.jl_typeof_str.restype = ctypes.c_char_p
+
+        unbox_map = dict(float32 = ctypes.c_float,
+                         float64 = ctypes.c_double,
+                         int32 = ctypes.c_int,
+                         int64 = ctypes.c_longlong,
+                         )
+
+        j_unboxers = {}
+        for jname, ctype in unbox_map.iteritems():
+            junboxer = getattr(j, 'jl_unbox_' + jname)
+            junboxer.restype = ctype
+            j_unboxers[jname] = junboxer
+
+        self._junboxers = j_unboxers
+        self._j = j
+
+    @line_cell_magic
+    def julia0(self, line, cell=None):
+        '''
+        Execute code in Julia, and pull some of the results back into the
+        Python namespace.
+        '''
+        src = str(line if cell is None else cell)
+        ans = self._j.jl_eval_string(src)
+        anstype = self._j.jl_typeof_str(ans).lower()
+        #print 'anstype:', anstype
+        try:
+            unboxer = self._junboxers[anstype]
+        except KeyError:
+            print "Unboxer not found for return type:", anstype  # dbg
+            return None
+        else:
+            return unboxer(ans)
+
+
+
+
+@magics_class
 class JuliaMagics(Magics):
     """A set of magics useful for interactive work with Julia.
     """
@@ -45,25 +109,18 @@ class JuliaMagics(Magics):
             j = sys._julia_initialized
         else: 
             j = ctypes.CDLL('libjulia-release.so', ctypes.RTLD_GLOBAL)
-            j.jl_init()
+            j.jl_init("/home/fperez/tmp/src/julia/usr/lib")
             sys._julia_initialized = j
+            
+            j.jl_eval_string('using PyCall;pyinitialize("%s")' %
+            sys.executable)
+            jpyobj = j.jl_eval_string('PyObject')
        
         j.jl_typeof_str.restype = ctypes.c_char_p
+        j.jl_unbox_voidpointer.restype = ctypes.py_object
 
-        unbox_map = dict(float32 = ctypes.c_float,
-                         float64 = ctypes.c_double,
-                         int32 = ctypes.c_short,
-                         int64 = ctypes.c_int,
-                         )
-
-        j_unboxers = {}
-        for jname, ctype in unbox_map.iteritems():
-            junboxer = getattr(j, 'jl_unbox_' + jname)
-            junboxer.restype = ctype
-            j_unboxers[jname] = junboxer
-
-        self._junboxers = j_unboxers
         self._j = j
+        self._j_py_obj = jpyobj
 
     @line_cell_magic
     def julia(self, line, cell=None):
@@ -71,17 +128,11 @@ class JuliaMagics(Magics):
         Execute code in Julia, and pull some of the results back into the
         Python namespace.
         '''
+        j = self._j
         src = str(line if cell is None else cell)
         ans = self._j.jl_eval_string(src)
-        anstype = self._j.jl_typeof_str(ans).lower()
-        #print 'anstype:', anstype
-        try:
-            unboxer = self._junboxers[anstype]
-        except KeyError:
-            #print "Unboxer not found for return type:", anstype  # dbg
-            return None
-        else:
-            return unboxer(ans)
+        pyans = j.jl_get_field(j.jl_call1(self._j_py_obj, ans), 'o')
+        return j.jl_unbox_voidpointer(pyans)
 
 
 __doc__ = __doc__.format(
@@ -91,4 +142,5 @@ __doc__ = __doc__.format(
 
 def load_ipython_extension(ip):
     """Load the extension in IPython."""
+    #ip.register_magics(JuliaMagics0)
     ip.register_magics(JuliaMagics)
