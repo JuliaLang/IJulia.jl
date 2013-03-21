891f3d07bb44bad8a36570fa56c4cc01612fc82e
Steven G. Johnson <stevenj@mit.edu>
2013-03-21 17:04:37 -0400

    delete obsole magics; use ctypes.util.find_library so that correct extension gets added to shared lib; explicit ctypes.c_void_p calls seem needed on MacOS to pass pointer arguments correctly (not sure why??)

--- juliamagic.py
+++ juliamagic.py
@@ -12,6 +12,7 @@
 # once, so this can actually be reloaded.
 
 import ctypes
+import ctypes.util
 import sys
 import commands
 
@@ -21,69 +22,6 @@ from IPython.core.magic import ( Magics, magics_class,
 
 class JuliaMagicError(Exception):
     pass
-
-###########################################################################
-# Original magics hack (limited type conversions)
-@magics_class
-class JuliaMagics0(Magics):
-    """Original set of limited magics useful for interactive work with Julia.
-    """
-    def __init__(self, shell):
-        """
-        Parameters
-        ----------
-        shell : IPython shell
-
-        """
-
-        global _julia_initialized
-        
-        super(JuliaMagics0, self).__init__(shell)
-
-        # Ugly hack to register the julia interpreter globally so we can reload
-        # this extension without trying to re-open the shared lib, which kills
-        # the python interpreter.  Nasty but useful while debugging
-        if hasattr(sys, '_julia_initialized'):
-            j = sys._julia_initialized
-        else: 
-            j = ctypes.CDLL('libjulia-release.so', ctypes.RTLD_GLOBAL)
-            j.jl_init("/home/fperez/tmp/src/julia/usr/lib")
-            sys._julia_initialized = j
-       
-        j.jl_typeof_str.restype = ctypes.c_char_p
-
-        unbox_map = dict(float32 = ctypes.c_float,
-                         float64 = ctypes.c_double,
-                         int32 = ctypes.c_int,
-                         int64 = ctypes.c_longlong,
-                         )
-
-        j_unboxers = {}
-        for jname, ctype in unbox_map.iteritems():
-            junboxer = getattr(j, 'jl_unbox_' + jname)
-            junboxer.restype = ctype
-            j_unboxers[jname] = junboxer
-
-        self._junboxers = j_unboxers
-        self._j = j
-
-    @line_cell_magic
-    def julia(self, line, cell=None):
-        '''
-        Execute code in Julia, and pull some of the results back into the
-        Python namespace.
-        '''
-        src = str(line if cell is None else cell)
-        ans = self._j.jl_eval_string(src)
-        anstype = self._j.jl_typeof_str(ans).lower()
-        #print 'anstype:', anstype
-        try:
-            unboxer = self._junboxers[anstype]
-        except KeyError:
-            #print "Unboxer not found for return type:", anstype  # dbg
-            return None
-        else:
-            return unboxer(ans)
 
 ###########################################################################
 # Julia magics using Julia PyCall module to perform type conversions
@@ -116,13 +54,15 @@ class JuliaMagics(Magics):
         status, JULIA_HOME = commands.getstatusoutput('julia -e "print(JULIA_HOME)"')
         if status != 0:
             raise JuliaMagicError("error executing julia command")
-        j = ctypes.PyDLL('%s/../lib/libjulia-release.so' % JULIA_HOME, 
-                         ctypes.RTLD_GLOBAL)
+        j = ctypes.PyDLL(ctypes.util.find_library('%s/../lib/libjulia-release' % JULIA_HOME), ctypes.RTLD_GLOBAL)
         print 'Initializing Julia...'
         j.jl_init('%s/../lib' % JULIA_HOME)
         sys._julia_initialized = j
 
         self._j = j
+        j.jl_eval_string.restype = ctypes.c_void_p
+        j.jl_call1.restype = ctypes.c_void_p
+        j.jl_get_field.restype = ctypes.c_void_p
         j.jl_typeof_str.restype = ctypes.c_char_p
         j.jl_unbox_voidpointer.restype = ctypes.py_object
         
@@ -134,7 +74,7 @@ class JuliaMagics(Magics):
 
     def jcall(self, src):
         ans = self._j.jl_eval_string(src)
-        if self._j.jl_typeof_str(ans) == "ErrorException":
+        if self._j.jl_typeof_str(ctypes.c_void_p(ans)) == "ErrorException":
             raise JuliaMagicError("ErrorException in Julia: %s" %src)
         else:
             return ans
@@ -149,8 +89,8 @@ class JuliaMagics(Magics):
         tstr = self._j.jl_typeof_str
         src = str(line if cell is None else cell)
         ans = self.jcall(src)
-        xx = j.jl_call1(self._j_py_obj, ans)
-        pyans = j.jl_unbox_voidpointer(j.jl_get_field(xx, 'o'))
+        xx = j.jl_call1(ctypes.c_void_p(self._j_py_obj), ctypes.c_void_p(ans))
+        pyans = j.jl_unbox_voidpointer(ctypes.c_void_p(j.jl_get_field(ctypes.c_void_p(xx), 'o')))
         ctypes.pythonapi.Py_IncRef(ctypes.py_object(pyans))
         return pyans
 
