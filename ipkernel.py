63b76628c9b7d42523f2498ce1366e91d9d2afda
Fernando Perez <Fernando.Perez@berkeley.edu>
2013-03-22 18:17:14 -0400

    Add local patch for IPython in our subclasss, will fix in ipython later

--- ipkernel.py
+++ ipkernel.py
@@ -6,6 +6,7 @@ from IPython.kernel.zmq.ipkernel import Kernel
 
 from IPython.utils.traitlets import DottedObjectName
 
+from zmq.eventloop.zmqstream import ZMQStream
 
 class JuliaKernel(Kernel):
     def execute_request(self, stream, ident, parent):
@@ -16,6 +17,23 @@ class JuliaKernelApp(IPKernelApp):
     name = 'juliakernel'
     kernel_class = DottedObjectName('juliamagic.ipkernel.JuliaKernel')
     kernel_class2 = JuliaKernel
+
+    def init_kernel(self):
+        """Create the Kernel object itself"""
+        shell_stream = ZMQStream(self.shell_socket)
+
+        Kernel = self.kernel_class2
+        
+        kernel = Kernel(config=self.config, session=self.session,
+                                shell_streams=[shell_stream],
+                                iopub_socket=self.iopub_socket,
+                                stdin_socket=self.stdin_socket,
+                                log=self.log,
+                                profile_dir=self.profile_dir,
+        )
+        kernel.record_ports(self.ports)
+        self.kernel = kernel
+
 
 
 def main():
