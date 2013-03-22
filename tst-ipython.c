3169aa56eb3b4407874b6747bf2556906e1f66f0
Steven G. Johnson <stevenj@alum.mit.edu>
2013-03-22 18:04:24 -0400

    aha, sys.argv needed

--- tst-ipython.c
+++ tst-ipython.c
@@ -3,8 +3,12 @@
 
 int main() {
         Py_InitializeEx(0) ;
+		PyObject *sys = PyImport_ImportModule("sys");
+		PyObject *o = Py_BuildValue("[s]", "python");
+		PyModule_AddObject(sys, "argv", o);
         if (PyImport_ImportModule("IPython")==NULL){
 		printf("NULL\n");
+		formatPythonError();
 	}
     	return 0 ;
 }
