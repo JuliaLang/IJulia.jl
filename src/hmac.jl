e27972202e8f1584b147065acd3d336f67f808a2
Stefan Karpinski <stefan@karpinski.org>
2013-07-29 17:20:26 -0400

    hmac: default to SHA-256; handle empty string as signature_scheme.
    See https://github.com/ipython/ipython/commit/0f43d86ac8f05f8362e7979729c93822d721f065
    
    That commit makes SHA-256 the default HMAC hash function (good), and
    sometimes sets signature scheme to an empty string to indicate that
    no pefered signature scheme is set (less good).

--- src/hmac.jl
+++ src/hmac.jl
@@ -3,7 +3,9 @@ using GnuTLS
 if isempty(profile["key"])
     hmac(s1,s2,s3,s4) = ""
 else
-    signature_scheme = split(get(profile, "signature_scheme", "hmac-md5"), "-")
+    signature_scheme = get(profile, "signature_scheme", "hmac-sha256")
+    isempty(signature_scheme) && (signature_scheme = "hmac-sha256")
+    signature_scheme = split(signature_scheme, "-")
     if signature_scheme[1] != "hmac" || length(signature_scheme) != 2
         error("unrecognized signature_scheme")
     end
