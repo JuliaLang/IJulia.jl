b55cccf2a25308565062f583faaf221f71664fce
Steven G. Johnson <stevenj@alum.mit.edu>
2013-08-10 16:48:48 -0400

    implement ;... shell-escaping (fixes #43)

--- src/execute_request.jl
+++ src/execute_request.jl
@@ -86,10 +86,11 @@ execute_msg = nothing
 # note: 0x535c5df2 is a random integer to make name collisions in
 # backtrace analysis less likely.
 function execute_request_0x535c5df2(socket, msg)
-    @vprintln("EXECUTING ", msg.content["code"])
+    code = msg.content["code"]
+    @vprintln("EXECUTING ", code)
     global execute_msg = msg
     global _n, In, Out, _, __, ___, ans
-    silent = msg.content["silent"] || ismatch(r";\s*$", msg.content["code"])
+    silent = msg.content["silent"] || ismatch(r";\s*$", code)
 
     # present in spec but missing from notebook's messages:
     store_history = get(msg.content, "store_history", !silent)
@@ -97,20 +98,24 @@ function execute_request_0x535c5df2(socket, msg)
     if !silent
         _n += 1
         if store_history
-            In[_n] = msg.content["code"]
+            In[_n] = code
         end
         send_ipython(publish, 
                      msg_pub(msg, "pyin",
                              ["execution_count" => _n,
-                              "code" => msg.content["code"]]))
+                              "code" => code]))
     else
         @vprintln("SILENT")
     end
 
     send_status("busy")
 
+    # "; ..." lines are interpreted as shell commands for run
+    code = replace(code, r"^\s*;(.*)$"m, 
+                   m -> string(replace(m, r"^\s*;", "run(`"), "`)"), 0)
+
     try 
-        ans = result = include_string(msg.content["code"], "In[$_n]")
+        ans = result = include_string(code, "In[$_n]")
         if silent
             result = nothing
         elseif result != nothing
