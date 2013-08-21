2989cc1faecdbdc3a9c065ed193f07929c728e64
Steven G. Johnson <stevenj@mit.edu>
2013-08-21 16:09:58 -0400

    rows and cols to display should not depend on terminal where IJulia kernel is started; pick a sensible default (see JuliaLang/julia#4117)

--- src/kernel.jl
+++ src/kernel.jl
@@ -8,6 +8,11 @@ using IJulia
 
 ccall(:jl_install_sigint_handler, Void, ())
 
+# the size of truncated output to show should not depend on the terminal
+# where the kernel is launched, since the display is elsewhere
+ENV["LINES"] = 30
+ENV["COLUMNS"] = 80
+
 println(IJulia.orig_STDOUT, "Starting kernel event loops.")
 IJulia.watch_stdio()
 for sock in (IJulia.requests, IJulia.control)
