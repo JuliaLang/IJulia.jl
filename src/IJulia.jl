8602384b1f8f679cb27238947b329f53d540311c
Steven G. Johnson <stevenj@alum.mit.edu>
2016-08-23 08:01:22 -0400

    fix nested-list display in IJulia help, at least in Julia 0.5

--- src/IJulia.jl
+++ src/IJulia.jl
@@ -14,23 +14,18 @@ The `IJulia` module is used in three ways:
 * Typing `using IJulia; notebook()` will launch the Jupyter notebook
   interface in your web browser.  This is an alternative to launching
   `jupyter notebook` directly from your operating-system command line.
-
 * In a running notebook, the `IJulia` module is loaded and `IJulia.somefunctions`
   can be used to interact with the running IJulia kernel:
 
   - `IJulia.load(filename)` and `IJulia.load_string(s)` load the contents
     of a file or a string, respectively, into a notebook cell.
-
   - `IJulia.clear_output()` to clear the output from the notebook cell,
     useful for simple animations.
-
   - `IJulia.clear_history()` to clear the history variables `In` and `Out`.
-
   - `push_X_hook(f)` and `pop_X_hook(f)`, where `X` is either
     `preexecute`, `postexecute`, or `posterror`.  This allows you to
     insert a "hook" function into a list of functions to execute
     when notebook cells are evaluated.
-
   - `IJulia.set_verbose()` enables verbose output about what IJulia
     is doing internally; this is mainly used for debugging.
 
