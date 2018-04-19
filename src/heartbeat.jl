5715e95bef5ebf1da95067cada8c9036cdbeeb08
Steven G. Johnson <stevenj@mit.edu>
2018-04-19 19:23:23 -0400

    require ZMQ 0.6 for upcoming 0.7 support

--- src/heartbeat.jl
+++ src/heartbeat.jl
@@ -1,24 +1,14 @@
 # Spawn a thread (using pthreads on Unix/OSX and Windows threads on Windows)
 # to implement the "heartbeat" message channel, which is just a ZMQ
-# socket that echoes all messages.  This is implemented with the zmq_device
+# socket that echoes all messages.  This is implemented with the zmq_proxy
 # call in libzmq, which simply blocks forever, so the usual lack of
 # thread safety in Julia should not be an issue here.
 
-const libzmq = isdefined(ZMQ, :libzmq) ? ZMQ.libzmq : ZMQ.zmq
-
 # entry point for new thread
-if ZMQ.version ≥ v"3.2.1" # always true once we require a newer ZMQ.jl
-    function heartbeat_thread(sock::Ptr{Void})
-        ccall((:zmq_proxy,libzmq), Cint, (Ptr{Void}, Ptr{Void}, Ptr{Void}),
-              sock, sock, C_NULL)
-        nothing
-    end
-else
-    function heartbeat_thread(sock::Ptr{Void})
-        ccall((:zmq_device,libzmq), Cint, (Cint, Ptr{Void}, Ptr{Void}),
-              ZMQ.QUEUE, sock, sock)
-        nothing
-    end
+function heartbeat_thread(sock::Ptr{Void})
+    ccall((:zmq_proxy,ZMQ.libzmq), Cint, (Ptr{Void}, Ptr{Void}, Ptr{Void}),
+          sock, sock, C_NULL)
+    nothing
 end
 
 function start_heartbeat(sock)
