ad9348cda1478d31ebcefb64a9be328b20ec1439
Steven G. Johnson <stevenj@mit.edu>
2019-01-06 19:57:23 -0500

    more build logic improvements

--- deps/build.jl
+++ deps/build.jl
@@ -14,20 +14,18 @@ prefsfile = joinpath(first(DEPOT_PATH), "prefs", "IJulia")
 mkpath(dirname(prefsfile))
 jupyter = get(ENV, "JUPYTER", isfile(prefsfile) ? readchomp(prefsfile) : Sys.isunix() && !Sys.isapple() ? "jupyter" : "")
 condajupyter = normpath(Conda.SCRIPTDIR, exe("jupyter"))
-if isempty(jupyter)
+if isempty(jupyter) || dirname(jupyter) == abspath(Conda.SCRIPTDIR)
     jupyter = condajupyter # will be installed if needed
-else
-    if dirname(jupyter) == abspath(Conda.SCRIPTDIR)
-        jupyter = condajupyter # will be installed if needed
+elseif isabspath(jupyter)
+    if !Sys.isexecutable(jupyter)
+        @warn("ignoring non-executable JUPYTER=$jupyter")
+        jupyter = condajupyter
     end
-    if isabspath(jupyter)
-        if !Sys.isexecutable(jupyter)
-            @warn("ignoring non-executable JUPYTER=$jupyter")
-            jupyter = condajupyter
-        end
-    elseif Sys.which(jupyter) === nothing
-        @warn("JUPYTER=$jupyter not found in PATH")
-    end
+elseif jupyter != basename(jupyter) # relative path
+    @warn("ignoring relative path JUPYTER=$jupyter")
+    jupyter = condajupyter
+elseif Sys.which(jupyter) === nothing
+    @warn("JUPYTER=$jupyter not found in PATH")
 end
 
 function write_if_changed(filename, contents)
