41efb53df4d023011001cf753307c822faf637be
Nathan Daly <nhdaly@gmail.com>
2019-07-08 14:12:12 -0400

    Change name from "julia-1.1" to "julia1" for compat.
    After the Julia 1.0 release, minor version changes are supposed to be
    non-breaking, but before this commit, upgrading julia would break your
    notebooks since the kernel can't be found. This commit changes IJulia to
    only use the major version number in the kernel name so that tests will
    succeed b/w minor julia releases.

--- deps/kspec.jl
+++ deps/kspec.jl
@@ -38,7 +38,7 @@ kerneldir() = joinpath(jupyter_data_dir(), "kernels")
 
 
 exe(s::AbstractString) = Sys.iswindows() ? "$s.exe" : s
- 
+
 """
     installkernel(name::AbstractString, options::AbstractString...;
                   specname::AbstractString,
@@ -68,7 +68,7 @@ function installkernel(name::AbstractString, julia_options::AbstractString...;
     debugdesc = ccall(:jl_is_debugbuild,Cint,())==1 ? "-debug" : ""
 
     # path of the Jupyter kernelspec directory to install
-    juliakspec = joinpath(kerneldir(), "$specname-$(VERSION.major).$(VERSION.minor)$debugdesc")
+    juliakspec = joinpath(kerneldir(), "$specname$(VERSION.major)$debugdesc")
     @info("Installing $name kernelspec in $juliakspec")
     rm(juliakspec, force=true, recursive=true)
     try
