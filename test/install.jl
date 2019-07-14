4a2c81d5007eb04cd646fc86541b062f590a7cb5
Nathan Daly <nhdaly@gmail.com>
2019-07-14 16:07:48 -0400

    Add tests for custom kernel name, version, and debugdesc

--- test/install.jl
+++ test/install.jl
@@ -1,6 +1,7 @@
 using Test
 import IJulia, JSON
 
+isdebug() = ccall(:jl_is_debugbuild,Cint,())==1
 @testset "installkernel" begin
     let kspec = IJulia.installkernel("ijuliatest", "-O3", "-p2",
                     env=Dict("FOO"=>"yes"), specname="Yef1rLr4kXKxq9rbEh3m")
@@ -10,12 +11,31 @@ import IJulia, JSON
             @test isfile(joinpath(kspec, "logo-32x32.png"))
             @test isfile(joinpath(kspec, "logo-64x64.png"))
             let k = open(JSON.parse, joinpath(kspec, "kernel.json"))
-                debugdesc = ccall(:jl_is_debugbuild,Cint,())==1 ? "-debug" : ""
+                debugdesc = isdebug() ? "-debug" : ""
                 @test k["display_name"] == "ijuliatest" * " " * Base.VERSION_STRING * debugdesc
                 @test k["argv"][end] == "{connection_file}"
                 @test k["argv"][end-3:end-2] == ["-O3", "-p2"]
                 @test k["language"] == "julia"
                 @test k["env"]["FOO"] == "yes"
+            end
+        finally
+            rm(kspec, force=true, recursive=true)
+        end
+    end
+end
+@testset "installkernel -- custom names" begin
+    let kspec = IJulia.installkernel("ijuliatest", "-O3", "-p2",
+                    env=Dict("FOO"=>"yes"),
+                    specname="Yef1rLr4kXKxq9rbEh3m",
+                    specversion="v1",
+                    specdebugdesc=isdebug() ? "dbg" : "")
+        try
+            debugdesc = isdebug() ? "-dbg" : ""
+            @test dirname(kspec) == IJulia.kerneldir()
+            @test basename(kspec) == "Yef1rLr4kXKxq9rbEh3m-v1$debugdesc"
+            @test isfile(joinpath(kspec, "kernel.json"))
+            let k = open(JSON.parse, joinpath(kspec, "kernel.json"))
+                @test k["display_name"] == "ijuliatest" * " " * Base.VERSION_STRING * debugdesc
             end
         finally
             rm(kspec, force=true, recursive=true)
