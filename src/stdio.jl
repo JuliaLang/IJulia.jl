0263607e3e00f7d1ff891565930872a58e259996
Randy Gobbel <gobbel@acm.org>
2021-02-22 13:42:01 -0800

    No metaprogramming required with new functionality in redirect_stdio methods. Fixes #986.

--- src/stdio.jl
+++ src/stdio.jl
@@ -23,16 +23,6 @@ Base.displaysize(io::IJuliaStdio) = displaysize(io.io)
 Base.unwrapcontext(io::IJuliaStdio) = Base.unwrapcontext(io.io)
 Base.setup_stdio(io::IJuliaStdio, readable::Bool) = Base.setup_stdio(io.io.io, readable)
 
-for s in ("stdout", "stderr", "stdin")
-    f = Symbol("redirect_", s)
-    sq = QuoteNode(Symbol(s))
-    @eval function Base.$f(io::IJuliaStdio)
-        io[:jupyter_stream] != $s && throw(ArgumentError(string("expecting ", $s, " stream")))
-        Core.eval(Base, Expr(:(=), $sq, io))
-        return io
-    end
-end
-
 # logging in verbose mode goes to original stdio streams.  Use macros
 # so that we do not even evaluate the arguments in no-verbose modes
 
