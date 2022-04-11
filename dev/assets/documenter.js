d8882e89a8456dbcd9a8040a0daef26a9c0d9713
Documenter.jl <documenter@juliadocs.github.io>
2022-04-11 14:13:32 +0000

    build based on 2dc1b51

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -72,6 +72,67 @@ $(document).ready(function() {
 
 })
 ////////////////////////////////////////////////////////////////////////////////
+require([], function() {
+function addCopyButtonCallbacks() {
+  for (const el of document.getElementsByTagName("pre")) {
+    const button = document.createElement("button");
+    button.classList.add("copy-button", "fas", "fa-copy");
+    el.appendChild(button);
+
+    const success = function () {
+      button.classList.add("success", "fa-check");
+      button.classList.remove("fa-copy");
+    };
+
+    const failure = function () {
+      button.classList.add("error", "fa-times");
+      button.classList.remove("fa-copy");
+    };
+
+    button.addEventListener("click", function () {
+      copyToClipboard(el.innerText).then(success, failure);
+
+      setTimeout(function () {
+        button.classList.add("fa-copy");
+        button.classList.remove("success", "fa-check", "fa-times");
+      }, 5000);
+    });
+  }
+}
+
+function copyToClipboard(text) {
+  // clipboard API is only available in secure contexts
+  if (window.navigator && window.navigator.clipboard) {
+    return window.navigator.clipboard.writeText(text);
+  } else {
+    return new Promise(function (resolve, reject) {
+      try {
+        const el = document.createElement("textarea");
+        el.textContent = text;
+        el.style.position = "fixed";
+        el.style.opacity = 0;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand("copy");
+
+        resolve();
+      } catch (err) {
+        reject(err);
+      } finally {
+        document.body.removeChild(el);
+      }
+    });
+  }
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", addCopyButtonCallbacks);
+} else {
+  addCopyButtonCallbacks();
+}
+
+})
+////////////////////////////////////////////////////////////////////////////////
 require(['jquery', 'headroom', 'headroom-jquery'], function($, Headroom) {
 
 // Manages the top navigation bar (hides it when the user starts scrolling down on the
