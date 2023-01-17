b304e6eb3384dd9802161854eb62d29b48ec428f
hyrodium <hyrodium@gmail.com>
2023-01-18 01:16:45 +0900

    update path variables

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,11 @@
 using Documenter, IJulia
 
 # Copy assets from `deps` directory
-mkpath(joinpath(@__DIR__, "src/assets"))
-cp(joinpath(@__DIR__, "../deps/ijuliafavicon.ico"), joinpath(@__DIR__, "src/assets/favicon.ico"), force=true)
-cp(joinpath(@__DIR__, "../deps/ijulialogo.svg"), joinpath(@__DIR__, "src/assets/logo.svg"), force=true)
+path_assets = joinpath(@__DIR__, "src/assets")
+path_deps = joinpath(@__DIR__, "../deps")
+mkpath(path_assets)
+cp(joinpath(path_deps, "/ijuliafavicon.ico"), joinpath(path_assets, "favicon.ico"), force=true)
+cp(joinpath(path_deps, "/ijulialogo.svg"), joinpath(path_assets, "logo.svg"), force=true)
 
 # Make docs to `docs/build` directory
 makedocs(
