2ee9b73b54aa9196ed3d06eedb17da0e551083e5
JamesWrigley <james@puiterwijk.org>
2025-01-30 15:39:30 +0100

    Add Aqua to the test suite
    Turns out we commit type piracy on `Base.display()`, so the piracy test is
    disabled for now.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
+import Aqua
+import IJulia
+
 const TEST_FILES = [
     "install.jl", "comm.jl", "msg.jl", "execute_request.jl", "stdio.jl",
-    "inline.jl",
+    "inline.jl"
 ]
 
 for file in TEST_FILES
     println(file)
     include(file)
 end
+
+Aqua.test_all(IJulia; piracies=(; broken=true))
