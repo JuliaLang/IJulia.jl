ea037534afb30f2ad472d1c92d16d9defd6924f7
JamesWrigley <james@puiterwijk.org>
2025-02-08 18:44:39 +0100

    Send shutdown_reply on the control socket for protocol 5.4
    This is the only change in 5.4, in previous versions it was sent on the shell
    socket.

--- src/handlers.jl
+++ src/handlers.jl
@@ -225,7 +225,9 @@ function shutdown_request(socket, kernel, msg)
     # stop heartbeat thread
     stop_heartbeat(kernel)
 
-    send_ipython(kernel.requests[], kernel,
+    # In protocol 5.4 the shutdown reply moved to the control socket
+    shutdown_socket = VersionNumber(msg) >= v"5.4" ? kernel.control[] : kernel.requests[]
+    send_ipython(shutdown_socket, kernel,
                  msg_reply(msg, "shutdown_reply", msg.content))
     sleep(0.1) # short delay (like in ipykernel), to hopefully ensure shutdown_reply is sent
     kernel.shutdown()
