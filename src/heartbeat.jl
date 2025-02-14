130fdb863e114c3377e78d19abdf84c724de039d
JamesWrigley <james@puiterwijk.org>
2025-02-14 22:49:22 +0100

    Safely stop the heartbeat thread
    Previously we were not ensuring that the heartbeat thread had stopped before
    exiting, and were closing the context in an unsafe way. This was causing hangs
    on Mac and segfaults on Windows.

--- src/heartbeat.jl
+++ src/heartbeat.jl
@@ -23,7 +23,28 @@ function heartbeat_thread(heartbeat::Ptr{Cvoid})
 end
 
 function start_heartbeat(heartbeat, kernel)
+    heartbeat.linger = 0
     heartbeat_c = @cfunction(heartbeat_thread, Cint, (Ptr{Cvoid},))
     ccall(:uv_thread_create, Cint, (Ptr{Int}, Ptr{Cvoid}, Ptr{Cvoid}),
           kernel.heartbeat_threadid, heartbeat_c, heartbeat)
+end
+
+function stop_heartbeat(kernel)
+    if !isopen(kernel.heartbeat_context[])
+        # Do nothing if it has already been stopped (which can happen in the tests)
+        return
+    end
+
+    # First we call zmq_ctx_shutdown() to ensure that the zmq_proxy() call
+    # returns. We don't call ZMQ.close(::Context) directly because that
+    # currently isn't threadsafe:
+    # https://github.com/JuliaInterop/ZMQ.jl/issues/256
+    ZMQ.lib.zmq_ctx_shutdown(kernel.heartbeat_context[])
+    @ccall uv_thread_join(kernel.heartbeat_threadid::Ptr{Int})::Cint
+
+    # Now that the heartbeat thread has joined and its guaranteed to no longer
+    # be working on the heartbeat socket, we can safely close it and then the
+    # context.
+    close(kernel.heartbeat[])
+    close(kernel.heartbeat_context[])
 end
